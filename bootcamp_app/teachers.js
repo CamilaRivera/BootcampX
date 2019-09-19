51ee76c7dd84b5ebb7326613914586f2332ff8a3
CamilaRivera <camila.riverart@gmail.com>
2019-09-19 02:23:31 +0000

    Refactored 2 queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -11,16 +11,16 @@ const pool = new Pool({
 
 
 pool.query(`
-SELECT DISTINCT(teachers.name) AS name, cohorts.name AS cohort
-FROM assistance_requests
-JOIN teachers ON assistance_requests.teacher_id = teachers.id
-JOIN students ON assistance_requests.student_id = students.id
-JOIN cohorts ON cohorts.id = students.cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-ORDER BY teachers.name;
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+ORDER BY teacher;
 `)
 .then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.cohort}: ${user.name}`);
+  res.rows.forEach(row => {
+    console.log(`${row.cohort}: ${row.teacher}`);
   })
-}).catch(err => console.error('query error', err.stack));
+});
